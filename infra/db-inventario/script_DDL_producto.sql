1dafc58644378950784f0b671396beb5cac3a679
Char1 <soucharuno@gmail.com>
2026-06-08 17:17:42 -0400

    feature/contenedorizacion: Se definen los contenedores para el backend, el frontend, la bd y su respectivo docker-compose para levantar todo.

---
+++ infra/db-inventario/script_DDL_producto.sql
@@ -0,0 +1,19 @@
+
+-- Usamos la base de datos correcta por seguridad
+USE bd_cordillera_inventario;
+
+CREATE TABLE IF NOT EXISTS productos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(150) NOT NULL,
+    descripcion TEXT,
+    categoria VARCHAR(100),
+    marca VARCHAR(100),
+    sku VARCHAR(50) UNIQUE NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL DEFAULT 0,
+    stock_minimo INT NOT NULL DEFAULT 5,
+    calidad VARCHAR(50),
+    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    activo BOOLEAN DEFAULT TRUE
+);
